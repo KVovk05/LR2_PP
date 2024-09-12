05aa44fdc9b555e2a6f9b5c1e7a76e0e3b72bd89
KirilloXiX <vovk.kiryusha.007@gmail.com>
2024-09-12 22:17:14 +0300

    classes

---
+++ LR_Product/src/Main.java
@@ -0,0 +1,27 @@
+//Скласти масив об'єктів. Вивести:
+//        a) список товарів для заданого найменування;
+//        b) список товарів для заданого найменування, ціна яких не перевищує задану;
+//        c) список товарів, термін зберігання яких більше заданого.
+import java.util.Scanner;
+
+public class Main {
+    Product[] arr;
+
+    static void prodNaming() {
+        String name;
+        System.out.println("Enter naming of products which you want to output");
+        Scanner myObj = new Scanner(System.in);
+        name = myObj.toString();
+    }
+    static void namePrice(String name){
+
+    }
+    static void expDate(){
+
+    }
+    public static void main(String[] args) {
+    Product name;
+    Main newObj;
+    
+    }
+}
--- LR_Product/src/Product.java
+++ LR_Product/src/Product.java
@@ -1,2 +1,76 @@
+//методи setValue(), getValue(), toString()
 public class Product {
+    private int id;
+    private String naming;
+
+    public int getDaysToExp() {
+        return daysToExp;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", naming='" + naming + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", prodDate='" + prodDate + '\'' +
+                ", daysToExp=" + daysToExp +
+                ", price=" + price +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getNaming() {
+        return naming;
+    }
+
+    private String manufacturer;
+    private String prodDate;
+    private int daysToExp;
+    private double price;
+    private int amount;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNaming(String naming) {
+        this.naming = naming;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setProdDate(String prodDate) {
+        this.prodDate = prodDate;
+    }
+
+    public void setDaysToExp(int daysToExp) {
+        this.daysToExp = daysToExp;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Product(int id, String naming, String manufacturer, String prodDate, int daysToExp, double price, int amount) {
+        this.id = id;
+        this.naming = naming;
+        this.manufacturer = manufacturer;
+        this.prodDate = prodDate;
+        this.daysToExp = daysToExp;
+        this.price = price;
+        this.amount = amount;
+    }
+
 }
+
